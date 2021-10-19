c338daf1c1f14b4857be3852036e529ebee31494
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2021-10-19 14:05:58 +0200

    added appointment specs

---
+++ Domain/Tests/Fakes/Appointment.Fakes.cs
@@ -0,0 +1,20 @@
+﻿using FakeItEasy;
+using System;
+
+namespace Domain.Tests.Fakes
+{
+    public class AppointmentFakes
+    {
+        public class AppointmentDummyFactory : IDummyFactory
+        {
+            public Priority Priority => Priority.Default;
+
+            public bool CanCreate(Type type) => true;
+            
+
+            public object Create(Type type) =>
+                new Appointment()
+            
+        }
+    }
+}
---
+++ Domain/Tests/Specs/Appointment.Specs.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Domain.Tests
+{
+    public class AppointmentSpecs
+    {
+        [Fact]
+        public void CreateValidAppointment()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
