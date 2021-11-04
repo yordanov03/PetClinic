a3970f04e28d2fb7981d0e95e5c937e463673123
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2021-11-04 16:32:04 +0200

    removed constructor from UserInputModel

--- Application/Features/Identity/UserInputModel.cs
+++ Application/Features/Identity/UserInputModel.cs
@@ -2,11 +2,6 @@
 {
     public abstract class UserInputModel
     {
-        public UserInputModel(string email, string password)
-        {
-            this.Email = email;
-            this.Password = password;
-        }
         public string Email { get; set; } = default!;
 
         public string Password { get; set; } = default!;
